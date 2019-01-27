1723406527e9f1e1382a0982e6f829994e8a98b2
synleon <synleon@gmail.com>
2019-01-26 20:16:12 -0500

    nothing

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,18 +1,57 @@
 package com.example.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences prefs;
+    EditText editEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get reference for SharedPreferences
+        String key = getString(R.string.preference_file_key);
+        prefs = getSharedPreferences(key, Context.MODE_PRIVATE);
+
+        // get reserved email from SharedPreferences
+        key = getString(R.string.preference_reserved_email);
+        String email = prefs.getString(key, ""); ;
+
+        // push reserved email to edit control
+        editEmail = findViewById(R.id.editTextEmail);
+        editEmail.setText(email);
+
+        Button btnLogin = findViewById(R.id.login);
+
+        btnLogin.setOnClickListener( v -> {
+            Intent profilePage = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivityForResult(profilePage, 2);
+
+            // get email user typed
+
+
+        });
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
+        SharedPreferences.Editor editor  = prefs.edit();
+
+        String key = getString(R.string.preference_reserved_email);
+        String emailtyped = editEmail.getText().toString();
+        editor.putString(key, emailtyped);
+        editor.commit();
+
     }
 }
